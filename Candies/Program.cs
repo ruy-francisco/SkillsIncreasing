8759ec60d4a38c0a6fb8d82d36259db48ce2f4f8
Ruy Francisco Silva de Almeida <ruy.fsalmeida@gmail.com>
2017-06-12 11:46:02 -0300

    first solution

--- Candies/Program.cs
+++ Candies/Program.cs
@@ -8,8 +8,27 @@ namespace Candies
 {
     class Program
     {
+        public static int CountCandies(int startingAmount, int newEvery)
+        {
+            int EatenCandies = 0;
+
+            while(startingAmount >= newEvery)
+            {
+                EatenCandies += newEvery;
+                startingAmount -= newEvery;
+                startingAmount++;
+            }
+
+            EatenCandies += startingAmount;
+
+            return EatenCandies;
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine(CountCandies(5, 2));
+            Console.ReadKey();
         }
     }
 }
