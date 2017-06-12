5c06d8cc6a1ac834afd39cfb97328445d3a8b331
Ruy Francisco Silva de Almeida <ruy.fsalmeida@gmail.com>
2017-06-12 14:08:54 -0300

    First solution

---
+++ BirdsAndEggs/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdsAndEggs
+{
+    public interface IBird
+    {
+        Egg Lay();
+    }
+
+    public class Chicken: IBird
+    {
+        public Chicken()
+        {
+        }
+
+        public Egg Lay()
+        {
+            return new Egg(() =>
+            {
+                return new Chicken();
+            });
+        }
+    }
+
+    public class Egg
+    {
+        private static IBird Bird
+        {
+            get; set;
+        }
+
+        private static bool IsItHatched
+        {
+            get; set;
+        }
+
+        public Egg(Func<IBird> createdBird)
+        {
+            Bird = createdBird.Invoke();
+            IsItHatched = false;
+        }
+
+        public IBird Hatch()
+        {
+            if(!IsItHatched)
+            {
+                IsItHatched = true;
+                return Bird;
+            }            
+            else
+                throw new InvalidOperationException("This is egg was already hatched");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken1 = new Chicken();
+            var egg = chicken1.Lay();
+            var childChicken = egg.Hatch();
+
+            Console.ReadKey();
+        }
+    }
+}
