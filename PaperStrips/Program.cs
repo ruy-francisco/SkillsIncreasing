8b275e22f43d7867d1e7a029d7dbac11419a4243
Ruy Francisco Silva de Almeida <ruy.fsalmeida@gmail.com>
2017-06-14 10:19:38 -0300

    First solution

---
+++ PaperStrips/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperStrips
+{
+    public class Program
+    {
+        public static int MinPieces(int[] original, int[] desired)
+        {
+            int minPieces = 1;
+
+            var dictOriginal = original.Select((value, key) => new { value, key })
+                .ToDictionary(pair => pair.key, pair => pair.value);
+
+            for(int i = 0; i < original.Length - 1; i++)
+            {
+                int sumOfKeys = dictOriginal.First(d => d.Value == desired[i + 1]).Key - dictOriginal.First(d => d.Value == desired[i]).Key;
+                if(sumOfKeys != 1)
+                    minPieces++;
+            }
+
+            return minPieces;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] original = new int[] { 1, 4, 3, 2 };
+            int[] desired = new int[] { 1, 2, 4, 3 };
+            Console.WriteLine(MinPieces(original, desired));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PaperStrips_UT/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaperStrips;
+
+namespace PaperStrips_UT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MinPiecesTest()
+        {
+            Assert.AreEqual(3, PaperStrips.Program.MinPieces(new int[] { 1, 4, 3, 2 }, new int[] { 1, 2, 4, 3 }));
+            Assert.AreEqual(4, PaperStrips.Program.MinPieces(new int[] { 1, 5, 2, 4, 3 }, new int[] { 1, 3, 4, 5, 2}));
+            Assert.AreEqual(7, PaperStrips.Program.MinPieces(new int[] { 3, 4, 2, 9, 6, 1, 5, 7, 8 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+            Assert.AreEqual(3, PaperStrips.Program.MinPieces(new int[] { 1, 2, 5, 6, 3, 4 }, new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.AreEqual(3, PaperStrips.Program.MinPieces(new int[] { 1, 2, 5, 6, 3, 4, 7 }, new int[] { 1, 2, 3, 4, 7, 5, 6 }));
+        }
+    }
+}
